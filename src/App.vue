c88d12d31107f813264efc2f2210d551e8af1f02
L1n <89698082+l1n-zh@users.noreply.github.com>
2022-07-02 14:26:39 +0800

    RWD

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="absolute w-[100vw]">
-    <div class="flex flex-col bg-[#323232] w-[375px] h-[1500px] items-center">
+  <div class="absolute w-full flex justify-center bg-black" :style="{
+    zoom: Math.min(width/375, height/500),
+    transform: '',
+    transformOrigin: (0, 0),
+  }">
+    <div class="flex flex-col bg-[#323232] w-[375px] h-[1500px] items-center ">
       <transition name="fade">
         <router-view></router-view>
       </transition>
@@ -9,10 +13,20 @@
 </template>
 
 <script setup>
-import { ref } from "vue"
+import { ref, onMounted, onUnmounted } from "vue";
+
+const width = ref(window.innerWidth);
+const height = ref(window.innerHeight);
+function onResize() {
+  width.value = window.innerWidth;
+  height.value = window.innerHeight;
+}
+onMounted(() => window.addEventListener('resize', onResize))
+onUnmounted(() => window.removeEventListener('resize', onResize))
 </script>
 
 <style>
+
 @font-face {
   font-family: rajdhani;
   font-weight: normal;
