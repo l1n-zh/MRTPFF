29366abaca2bce41e29cec8426a43f2bfc65e28a
L1n <89698082+l1n-zh@users.noreply.github.com>
2022-07-03 15:39:11 +0800

    add transition

--- src/components/page/FieldDailyPredict.vue
+++ src/components/page/FieldDailyPredict.vue
@@ -1,22 +1,23 @@
 <template>
-    <div class="reference-line w-[297px] absolute h-[2px] rounded-[1px]" v-for="i in 6"
-      :style="{ bottom: (i - 1) * 30 + 21 + 'px' }"></div>
-    <div class="bg-[#272727] w-[68px] h-[35px] rounded-[8px] absolute top-[3px] left-[3px] shadow-md z-20 p-[2px]"
-      :style="{ left: hovered * 11 - 8 + 'px' }">
-      <monospaced :content="`${data[hovered - 1]}`" :size="25" color="#ffffff" />
-      <monospaced content="人" :size="1" color="#ffffff" />
-    </div>
-    <div class="flex justify-between w-full h-[180px] mt-auto z-10">
-      <svg :class="['flex w-[4px] h-full', hovered == n ? 'hight-light' : '']" @mouseover="mouseOver(n)"
-        v-for="n in 24">
-        <color />
-        <clipPath :id="`clipPathD${n}`">
-          <rect :height="`${(data[n - 1] / max) * 100}%`" width="100%" :y="`${(1 - data[n - 1] / max) * 100}%`" rx="2px"
-            ry="2px" />
-        </clipPath>
-        <rect width="100%" height="100%" :clip-path="`url(#clipPathD${n})`" fill="url(#color)" />
-      </svg>
-    </div>
+  <div class="reference-line w-[297px] absolute h-[2px] rounded-[1px]" v-for="i in 6"
+    :style="{ bottom: (i - 1) * 30 + 21 + 'px' }"></div>
+  <div
+    class="bg-[#272727] w-[68px] h-[35px] rounded-[8px] absolute top-[3px] left-[3px] shadow-md z-20 p-[2px] bar-transition"
+    :style="{ left: hovered * 11 - 8 + 'px' }">
+    <monospaced :content="`${data[hovered - 1]}`" :size="25" color="#ffffff" />
+    <monospaced content="人" :size="1" color="#ffffff" />
+  </div>
+  <div class="flex justify-between w-full h-[180px] mt-auto z-10">
+    <svg :class="['flex w-[4px] h-full transition', hovered == n ? 'hight-light' : '']" @mouseover="mouseOver(n)"
+      v-for="n in 24">
+      <color />
+      <clipPath :id="`clipPathD${n}`">
+        <rect :height="`${(data[n - 1] / max) * 100}%`" width="100%" :y="`${(1 - data[n - 1] / max) * 100}%`" rx="2px"
+          ry="2px" />
+      </clipPath>
+      <rect width="100%" height="100%" :clip-path="`url(#clipPathD${n})`" fill="url(#color)" />
+    </svg>
+  </div>
 </template>
 
 <script setup>
@@ -38,6 +39,10 @@ function mouseOver(n) {
 </script>
 
 <style scoped>
+
+.bar-transition {
+  transition: all 60ms linear;
+}
 .reference-line {
   box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.1);
 }
