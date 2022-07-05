862e5d8d6f234b3542db7aeb085663adf4756931
L1n <89698082+l1n-zh@users.noreply.github.com>
2022-07-05 23:05:28 +0800

    bug fix

--- src/components/page/FieldWeeklyPredict.vue
+++ src/components/page/FieldWeeklyPredict.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col justify-between w-full h-full">
       <div class="h-[20px] w-full flex flex-row justify-between items-center" v-for="n in 7">
         <div class="text-[18px] leading-[18px] mr-1" :style="{ color: n == today ? 'white' : '#B3B3B3' }">
-          周一
+          {{"周" + "一二三四五六日"[n-1]}}
         </div>
 
         <div class="divider w-[2px] h-[80%] rounded-[1px] bg-[#666666] mr-1" />
@@ -13,11 +13,8 @@
           <svg class="bg-[#272727] w-full h-full rounded-[1px] shadow">
             <def>
               <color />
-              <clipPath id="f">
-                <rect height="100%" width="50%" />
-              </clipPath>
             </def>
-            <rect height="100%" width="50%" clip-path="url(#f)" fill="url(#color)" rx="1px" ry="1px" />
+            <rect height="100%" width="80%" fill="url(#color)" rx="1px" ry="1px" />
           </svg>
         </div>
         <m-text content="1200" :size="25" :color="n == today ? 'white' : '#B3B3B3'" class="ml-2" />
