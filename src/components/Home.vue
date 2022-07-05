7f5f51dcb8c05dde8769b4d94c065c18b403fe87
L1n <89698082+l1n-zh@users.noreply.github.com>
2022-07-05 23:02:57 +0800

    bug fix(選單無法關閉)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,45 @@
 <template>
-    <div class="fixed h-auto z-50 w-[375px] h-max flex flex-col items-center bg-[#212121] shadow-md">
-        <text class="text-white mt-[24px] text-[25px]"> 捷運人數預測 </text>
-        <search-bar></search-bar>
-    </div>
-    <div class="mt-[137.5px]"/>
-    <panel station="大安森林公園" :time="12" :entered="2100" :exited="2000" />
-    <panel station="大安森林公園" :time="12" :entered="2100" :exited="2000" />
+  <div
+    class="background fixed z-50 w-[375px] flex flex-col items-center"
+    :style="{
+      backdropFilter: scrolled || activated ? 'brightness(.6) blur(16px)' : '',
+      height: activated ? '100%' : 'auto',
+    }"
+  >
+    <text class="text-white mt-[24px] text-[25px]" v-if="!activated">
+      捷運人數預測
+    </text>
+    <search-bar></search-bar>
+    <div class="w-full flex flex-grow" @click="deactivate()"></div>
+  </div>
+  <div class="mt-[137.5px]" />
+  <panel station="大安森林公園" :time="12" :entered="2100" :exited="2000" />
+  <panel station="大安森林公園" :time="12" :entered="2100" :exited="2000" />
 </template>
 
 <script setup>
 import panel from "./panel.vue";
-import SearchBar from "./TheSearchBar.vue"
+import SearchBar from "./TheSearchBar.vue";
+import { ref, onMounted, onUnmounted, computed } from "vue";
+import { useStore } from "vuex";
 
+const store = useStore();
+const activated = computed(() => store.state.activated);
+const scrolled = ref(0);
+
+const deactivate = () => store.commit("activate", false);
+
+onMounted(() => {
+  window.addEventListener("scroll", onScroll);
+});
+onUnmounted(() => window.removeEventListener("scroll", onScroll));
+function onScroll() {
+  scrolled.value = window.scrollY > 10;
+}
 </script>
+
+<style scoped>
+.background {
+  transition: all 120ms linear;
+}
+</style>
