d7c0d8ee3039322f4ed6e7518b1c746aee8ac9ec
L1n <89698082+l1n-zh@users.noreply.github.com>
2022-07-08 01:34:24 +0800

    add FiledLoading

---
+++ src/components/page/FieldLoading.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex flex-col w-full h-full">
+        <div class="flex flex-col w-full mb-[20px]" v-for="i in 2">
+            <div class="bar" v-for="j in i+2" :style="{ width: Math.floor(Math.random() * 60 + 40) + '%' }"></div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="postcss">
+.bar {
+    @apply h-[8px] rounded-[2px] mt-[10px];
+    background: repeating-linear-gradient(to right, #323232, #434343, #323232);
+    background-size: 200% auto;
+    background-position: 0 100%;
+    animation: loading 1.5s linear infinite;
+}
+
+@keyframes loading {
+    from {
+        background-position: 0 0;
+    }
+
+    to {
+        background-position: -200% 0;
+    }
+}
+</style>
