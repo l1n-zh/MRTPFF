414f0b1066475915d89a61e001680cf49d1610b9
L1n <89698082+l1n-zh@users.noreply.github.com>
2022-07-08 01:33:43 +0800

    bug fix

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,14 @@
     }"
     @touchmove="onTouchmove"
   >
-    <div class="flex flex-col bg-[#323232] w-[375px] h-[1500px] items-center">
-      <transition name="fade">
-        <router-view></router-view>
-      </transition>
+    <div class="flex flex-col bg-[#323232] w-[375px] h-auto min-h-[1000px] pb-[30px] items-center">
+      <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted, computed } from "vue";
+import { onMounted, onUnmounted, computed } from "vue";
 import { useStore } from "vuex"
 
 const store = useStore()
@@ -48,21 +46,6 @@ onUnmounted(() => window.removeEventListener("resize", onResize))
 @font-face {
   font-family: rajdhani;
   font-weight: normal;
-  src: url("../assets/fonts/Rajdhani-Regular.ttf") format("truetype");
-}
-
-@font-face {
-  font-family: glow-sans;
-  src: url("../assets/fonts/GlowSansTC-Regular.otf") format("truetype");
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
+  src: url("../Rajdhani-Regular.ttf") format("truetype");
 }
 </style>
