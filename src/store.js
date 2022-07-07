a8537b42035158662d5e4863c5edda4cfa29e6e0
L1n <89698082+l1n-zh@users.noreply.github.com>
2022-07-08 01:33:24 +0800

    add default values

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { createStore } from "vuex"
 const state = {
     activated: false,
     zoomLevel: 1,
-    favorites: new Set()
+    favorites: new Set(["明德", "忠孝新生", "南港軟體園區", "市政府", "台北小巨蛋", "新北產業園區"])
 }
 
 const mutations = {
