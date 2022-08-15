79dd9500fdfc6c349b6f2896cb99fc19c4124f57
L1n <89698082+l1n-zh@users.noreply.github.com>
2022-08-15 13:47:37 +0800

    style fix

--- src/store.js
+++ src/store.js
@@ -1,27 +1,27 @@
-import { createStore } from "vuex"
+import { createStore } from "vuex";
 
 const state = {
     activated: false,
     zoomLevel: 1,
     favorites: new Set(["明德", "忠孝新生", "南港軟體園區", "市政府", "台北小巨蛋", "新北產業園區"])
-}
+};
 
 const mutations = {
     activate(state, status) {
-        state.activated = status
+        state.activated = status;
     },
     setZoomLevel(state, value) {
-        state.zoomLevel = value
+        state.zoomLevel = value;
     },
     addToFavorites(state, station) {
-        state.favorites.add(station)
+        state.favorites.add(station);
     },
     removeFromFavorites(state, station) {
-        state.favorites.delete(station)
+        state.favorites.delete(station);
     }
-}
+};
 
 export default createStore({
-  state,
-  mutations
-})
+    state,
+    mutations
+});
