57445555c16161b9aa1a3b33a2c13ad839860a7b
L1n <89698082+l1n-zh@users.noreply.github.com>
2022-08-21 15:36:05 +0800

    feat: 本周預測改為未來六天預測

--- src/components/page/FieldWeeklyPredict.vue
+++ src/components/page/FieldWeeklyPredict.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex flex-col justify-between w-full h-[210px]">
     <div class="h-[20px] w-full flex flex-row justify-between items-center" v-for="d, n in data">
-      <div class="text-[18px] leading-[18px] mr-1" :style="{ color: n == today ? 'white' : '#B3B3B3' }">
-        {{ "週" + "一二三四五六日"[n] }}
+      <div class="text-[18px] leading-[18px] mr-1" :style="{ color: n ? '#B3B3B3' : 'white' }">
+        {{ "週" + "日一二三四五六"[(today+n)%7] }}
       </div>
 
       <div class="w-[1px] h-[60%] rounded-[1px] bg-[#666666] mr-1" />
 
-      <MText :content="d.offPeak" :size="25" :color="n == today ? 'white' : '#B3B3B3'" class="mr-2" />
+      <MText :content="d.offPeak" :size="25" :color="n ? '#B3B3B3' : 'white'" class="mr-2" />
 
       <div class="w-0 h-[4px] flex-grow">
         <svg class="bg-[#272727] w-full h-full rounded-[1px]">
@@ -16,7 +16,7 @@
             ry="1px" />
         </svg>
       </div>
-      <MText :content="d.peak" :size="25" :color="n == today ? 'white' : '#B3B3B3'" class="ml-2" />
+      <MText :content="d.peak" :size="25" :color="n ? '#B3B3B3' : 'white'" class="ml-2" />
     </div>
   </div>
 </template>
@@ -27,8 +27,11 @@ import MText from "../appearance/MonospacedFontText.vue";
 const props = defineProps({
   data: Object
 })
+
+console.log(props.data)
 const minOffPeak = Math.min(...props.data.map(d => d.offPeak))
 const maxPeak = Math.max(...props.data.map(d => d.peak))
 
-const today = new Date().getDay() - 1;
+const today = new Date().getDay();
+console.log(today)
 </script>
