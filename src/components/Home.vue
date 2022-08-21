f87e5e7ad563ca30ffd1514cab9b9f9901501e56
L1n <89698082+l1n-zh@users.noreply.github.com>
2022-08-21 15:36:35 +0800

    add vueuse

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    class="background fixed z-50 w-[375px] flex flex-col items-center"
+    class="background fixed z-50 w-[375px] flex flex-col items-center overflow-hidden"
     :style="{
-      backdropFilter: scrolled || activated ? 'brightness(.6) blur(16px)' : '',
+      backdropFilter: y > 10 || activated ? 'brightness(.6) blur(16px)' : '',
       height: activated ? '100%' : 'auto',
     }"
   >
     <text class="text-white mt-[24px] text-[25px]" v-if="!activated">
       捷運人數預測
     </text>
-    <search-bar></search-bar>
-    <div class="w-full flex flex-grow" @click="deactivate()"></div>
+    <SearchBar></SearchBar>
+    <div class="w-full flex flex-grow h-full" @click="deactivate()"></div>
   </div>
   <div class="mt-[137.5px]" />
   <panel v-for="station in stations" :station="station" :time="new Date().getHours()" :entered="733" :exited="454" />
@@ -21,22 +21,15 @@ import panel from "./panel.vue";
 import SearchBar from "./TheSearchBar.vue";
 import { ref, onMounted, onUnmounted, computed } from "vue";
 import { useStore } from "vuex";
+import { useWindowScroll } from '@vueuse/core'
+
+const { x, y } = useWindowScroll()
 
 const store = useStore();
 const activated = computed(() => store.state.activated);
-const scrolled = ref(0);
 const stations = computed(() => store.state.favorites)
 
 const deactivate = () => store.commit("activate", false);
-
-onMounted(() => {
-  window.addEventListener("scroll", onScroll);
-  deactivate()
-});
-onUnmounted(() => window.removeEventListener("scroll", onScroll));
-function onScroll() {
-  scrolled.value = window.scrollY > 10;
-}
 </script>
 
 <style scoped>
